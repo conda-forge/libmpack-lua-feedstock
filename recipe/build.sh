4400525f5f8a5bfb3c6b474a640040b4d1591398
danielnachun <daniel.nachun@gmail.com>
2023-10-29 01:34:02 -0700

    fix cross compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,4 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-luarocks --tree ${PREFIX} make --lua-dir ${PREFIX} --deps-mode=none --no-manifest CC=${CC} 
+luarocks --tree ${PREFIX} make --lua-dir ${BUILD_PREFIX} --lua-version 5.1 --deps-mode=none --no-manifest CC=${CC} LD=${CC}
